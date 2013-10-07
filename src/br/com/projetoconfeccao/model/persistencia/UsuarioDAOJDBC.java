59b99f4dd9af7ad367316e03398bd2cad154ccad
gustavo_lourenco <gustavo_lourenco@10.1.48.220>
2013-10-07 15:14:47 -0300

    Teste

---
+++ src/br/com/projetoconfeccao/model/persistencia/UsuarioDAOJDBC.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.projetoconfeccao.model.persistencia;
+
+/**
+ *
+ * @author gustavo_lourenco
+ */
+public class UsuarioDAOJDBC {
+    
+}
---
+++ src/br/com/projetoconfeccao/model/persistencia/dao/UsuarioDAO.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.projetoconfeccao.model.persistencia.dao;
+
+/**
+ *
+ * @author gustavo_lourenco
+ */
+public interface UsuarioDAO {
+    
+}
